22d40f1eb68979c338758eed25c4e70ab6c71684
Samuel Cazaes <samuelcazaes@gmail.com>
2024-05-01 13:10:41 -0300

    adicionando miniaturas e suas animações

--- src/Components/projetos/Projetos.jsx
+++ src/Components/projetos/Projetos.jsx
@@ -3,13 +3,16 @@ import Tip from '../../Assets/icons/dica.svg?react'
 import styles from './Projetos.module.css'
 import Portfolio from '../../Assets/imagens/Projetos/on/samuelcazaes_on.png'
 import PortfolioOFF from '../../Assets/imagens/Projetos/off/samuelcazaes_off.png'
+import PortfolioMiniature from '../../Assets/imagens/Projetos/miniatures/portfoliominiature.jpg'
 import AgileTrack from '../../Assets/imagens/Projetos/on/agiletrack_on.png'
 import AgileTrackOFF from '../../Assets/imagens/Projetos/off/agiletrack_off.png'
+import AgileTrackMiniature from '../../Assets/imagens/Projetos/miniatures/agiletrackminiature.jpg'
 import VisiteQueluzito from '../../Assets/imagens/Projetos/on/visitequeluzito_on.png'
 import VisiteQueluzitoOFF from '../../Assets/imagens/Projetos/off/visitequeluzito_off.png'
+import VisiteQueluzitoMiniature from '../../Assets/imagens/Projetos/miniatures/visitequeluzitominiature.jpg'
 import AmarilFranklin from '../../Assets/imagens/Projetos/on/amarilfranklin_on.png'
 import AmarilFranklinOFF from '../../Assets/imagens/Projetos/off/amarilfranklin_off.png'
-import PortfolioMiniature from '../../Assets/imagens/Projetos/miniatura-projetos.jpg'
+import AmarilFranklinMiniature from '../../Assets/imagens/Projetos/miniatures/amarilfranklinminiature.jpg'
 import Setinha from '../../Assets/icons/seta_botao.svg?react'
 
 const Projetos = () => {
@@ -58,16 +61,20 @@ const Projetos = () => {
           <h1 className={styles.detailsProjetosTitle}>projetos<span>.</span></h1>
         </div>
         <div className={styles.miniatureBox}>
-          <img src={PortfolioMiniature} alt="" />
-        </div>
-        <div className={styles.textBox}>
+          {infoDisplay === 'portfolio' ? <img key={Math.random()} src={PortfolioMiniature} alt="" />
+          : infoDisplay === 'agileTrack' ? <img key={Math.random()} src={AgileTrackMiniature} alt="" />
+          : infoDisplay === 'visiteQueluzito' ? <img key={Math.random()} src={VisiteQueluzitoMiniature} alt="" />
+          : infoDisplay === 'amarilFranklin' && <img key={Math.random()} src={AmarilFranklinMiniature} alt="" />
+          }
           
+        </div>
+
+        <div className={styles.textBox}>         
           {infoDisplay === 'portfolio' ? <h2 key={Math.random()}>Portfólio</h2> 
           : infoDisplay === 'agileTrack' ? <h2 key={Math.random()}>Agile Track</h2>
           : infoDisplay === 'visiteQueluzito' ? <h2 key={Math.random()}>Visite Queluzito</h2>
           : infoDisplay === 'amarilFranklin' && <h2 key={Math.random()}>Amaril Franklin</h2>
           }
-        
           {infoDisplay === 'portfolio' ? 
             <div key={Math.random()} className={styles.descriptions}>
               <p>O projeto portfólio consiste em ser como um currículo virtual para o desenvolvedor,descrevendo habilidades e as comprovando por meio de uma lista de projetos aqui demonstrados,possibilitando o visitante saber qual fora a participação deste programador nos projetos mencionados com apenas um clique.
@@ -96,8 +103,7 @@ const Projetos = () => {
               <p>Este site foi projetado desde o começo visando a excelência, sendo assim ferramentas como o Figma foram necessárias para a prototipação e UI/UX, enquanto sua codificação fora feita em ReactJS e Git para versionamento, aplicando os conhecimentos adquiridos nos cursos concluídos.
               </p>
             </div>}
-        </div>
-        
+        </div>        
         <a href="">
           <div className={styles.button}>
             <span>SAIBA MAIS</span><Setinha/>
