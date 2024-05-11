27b618954deac3efcc307c4ba8f29e787247443f
Samuel Cazaes <samuelcazaes@gmail.com>
2024-05-11 13:22:02 -0300

    adicionando grupo extra curriculares na seção formações, falta idiomas

--- src/Components/formacoes/Formacoes.jsx
+++ src/Components/formacoes/Formacoes.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './Formacoes.module.css'
 import Decorador from '../../Assets/decoradores/decBolinhas.svg?react'
 import Capelo from '../../Assets/icons/academico.svg?react'
+import ExtraCurri from '../../Assets/icons/extracurriculares.svg?react'
+
 const Formacoes = () => {
   return (
     <section className={`${styles.formacoes} container`}>
@@ -10,7 +12,7 @@ const Formacoes = () => {
 
     <div className={styles.formacoesContainer}>
       <div className={styles.formacaoAcademica}>
-        <div className={styles.formacaoAcademicaTitle}>
+        <div className={styles.formacoesTitle}>
           <Capelo/>
           <h1>formação acadêmica</h1>
         </div>
@@ -21,10 +23,60 @@ const Formacoes = () => {
               <h1>Universidade Paulista / UNIP</h1>
             </div>
               <h2>Análise e Desenvolvimento de Sistemas</h2>
+            <div className={styles.decoradores}>
+              <Decorador/>
+              <Decorador/>
+            </div>
           </div>
           <div className={styles.formacaoAcademicaData}>
             <h2>08/21 - 07/23</h2>
             <span>EAD</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.extraCurriculares}>
+        <div className={styles.extraCurricularesTitle}>
+          <h1>extra curriculares</h1>
+          <ExtraCurri/>
+        </div>
+        <div className={styles.extraCurricularesBox}>
+          <div className={styles.extraCurricularesSchoolTitle}>
+            <span>{'>'}</span>
+            <h1>Origamid</h1>
+          </div>
+          <div className={styles.extraCurricularesCompetenciasBox}>
+            <div className={styles.extraCurricularesCompetencia}>
+              <h2>HTML e CSS para Iniciantes</h2>
+              <span>completo - 46 horas</span>
+            </div>
+            <div className={styles.extraCurricularesCompetencia}>
+              <h2>CSS Grid Layout</h2>
+              <span>completo - 10 horas</span>
+            </div>
+            <div className={styles.extraCurricularesCompetencia}>
+              <h2>CSS Flexbox</h2>
+              <span>completo - 6 horas</span>
+            </div>
+            <div className={styles.extraCurricularesCompetencia}>
+              <h2>JavaScript Completo ES6</h2>
+              <span>completo - 74 horas</span>
+            </div>
+            <div className={styles.extraCurricularesCompetencia}>
+              <h2>React Completo</h2>
+              <span>completo - 36 horas</span>
+            </div>
+            <div className={styles.extraCurricularesCompetencia}>
+              <h2>UI Design para Iniciantes</h2>
+              <span>completo - 34 horas</span>
+            </div>
+            <div className={styles.extraCurricularesCompetencia}>
+              <h2>UI Design Avançado</h2>
+              <span>a iniciar - 15 horas</span>
+            </div>
+            <div className={styles.extraCurricularesCompetencia}>
+              <h2>Tipografia Avançada</h2>
+              <span>a iniciar - 12 horas</span>
+            </div>
           </div>
         </div>
       </div>
